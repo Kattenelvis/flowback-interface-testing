66f8bb82a77c9c168294b3fee9b96e0e8d3b76d4
emil <53380638+Kattenelvis@users.noreply.github.com>
2026-05-24 13:05:21 +0200

    Fixed smoke

--- smoke.test.ts
+++ smoke.test.ts
@@ -5,10 +5,13 @@ import { login, logout, newWindow } from "./generic"
 test('Smoke-Test', async ({ page }) => {
   await login(page)
   const bPage = await newWindow()
+  const dPage = await newWindow()
   await login(bPage, { username: process.env.SECONDUSER_NAME, password: process.env.SECONDUSER_PASS })
   await logout(page)
   await login(page, { username: process.env.THIRDUSER_NAME, password: process.env.THIRDUSER_PASS })
   await logout(page)
+  await login(dPage, { username: process.env.FOURTHUSER_NAME, password: process.env.FOURTHUSER_PASS })
   await logout(bPage)
+  await logout(dPage)
 })
 
